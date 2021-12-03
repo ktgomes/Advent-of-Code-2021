e335f8a2aa807c1438149de6fc87cbd8bf3f748f
Katia <kgomes@niu.edu>
2021-12-03 09:25:48 -0600

    Day 3 progress

--- d03/p1.go
+++ d03/p1.go
@@ -1,5 +1,75 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
 func main() {
+
+	const columns int = 12
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Can't read file")
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var count [columns]int
+	var line string
+
+	for scanner.Scan() {
+		line = scanner.Text()
+
+		for i := 0; i < len(line); i++ {
+			if line[i] == '0' {
+				count[i] -= 1
+			} else {
+				count[i] += 1
+			}
+		}
+	}
+
+	var gamma []byte
+	for i := 0; i < len(count); i++ {
+		// >0 means there are more ones
+		if count[i] > 0 {
+			gamma = append(gamma, '1')
+		} else {
+			gamma = append(gamma, '0')
+		}
+
+	}
+
+	var epsilon []byte
+	for i := 0; i < len(count); i++ {
+		// <0 means there are more zeros
+		if count[i] < 0 {
+			epsilon = append(epsilon, '1')
+		} else {
+			epsilon = append(epsilon, '0')
+		}
+	}
+
+	gammaVal, err := strconv.ParseInt(string(gamma), 2, 64)
+	if err != nil {
+		fmt.Println("Error Parsing Binary")
+		panic(err)
+	}
+	fmt.Println(gammaVal)
+
+	epsilonVal, err := strconv.ParseInt(string(epsilon), 2, 64)
+	if err != nil {
+		fmt.Println("Error Parsing Binary")
+		panic(err)
+	}
+	fmt.Println(epsilonVal)
+
+	fmt.Println(gammaVal * epsilonVal)
 
 }
--- d03/p2.go
+++ d03/p2.go
@@ -1,5 +1,98 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
+
+	const columns int = 5
+
+	file, err := os.Open("test.txt")
+	if err != nil {
+		fmt.Println("Can't read file")
+		panic(err)
+	}
+	defer file.Close()
+
+	var originalLines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		originalLines = append(originalLines, scanner.Text())
+	}
+
+	oxygenLines := originalLines
+	fmt.Println(oxygenLines)
+
+	for i := 0; i < columns; i++ {
+		count := 0
+		for _, line := range oxygenLines {
+			if line[i] == '0' {
+				count -= 1
+			} else {
+				count += 1
+			}
+		}
+
+		var markForDeletion []int
+
+		for k, line := range oxygenLines {
+			// If 1 is most common, remove columns with 0s
+			if count >= 0 {
+				if line[i] == '0' {
+					markForDeletion = append(markForDeletion, k)
+				}
+			} else { // If 0 is most common, remove columns with 1s
+				if line[i] == '1' {
+					markForDeletion = append(markForDeletion, k)
+				}
+			}
+		}
+
+		for k, value := range markForDeletion {
+			oxygenLines = append(oxygenLines[:value-k], oxygenLines[value+1-k:]...)
+		}
+	}
+
+	oxygen := oxygenLines[0]
+	fmt.Println(oxygen)
+
+	co2Lines := originalLines
+	fmt.Println(co2Lines)
+
+	for i := 0; i < columns; i++ {
+		count := 0
+		for _, line := range co2Lines {
+			if line[i] == '0' {
+				count -= 1
+			} else {
+				count += 1
+			}
+		}
+
+		var markForDeletion []int
+
+		for k, line := range co2Lines {
+			// If 1 is least common, remove columns with 0s
+			if count <= 0 {
+				if line[i] == '0' {
+					markForDeletion = append(markForDeletion, k)
+				}
+			} else { // If 0 is least common, remove columns with 1s
+				if line[i] == '1' {
+					markForDeletion = append(markForDeletion, k)
+				}
+			}
+		}
+
+		for k, value := range markForDeletion {
+			co2Lines = append(co2Lines[:value-k], co2Lines[value+1-k:]...)
+		}
+	}
+
+	co2 := co2Lines[0]
+	fmt.Println(co2)
 
 }
