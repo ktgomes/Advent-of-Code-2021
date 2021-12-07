55d60566d7da3b9284b16140235e5b3dc1a0db4e
Katia <kgomes@niu.edu>
2021-12-06 23:28:31 -0600

    Day 6 part 2

--- d06/p2.go
+++ d06/p2.go
@@ -1,40 +1,41 @@
 package main
 
-type lanternfish struct {
-	days_until_birth int
-	Child            *lanternfish
-}
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
 
-const final_day int = 80
+const final_day int = 256
 
-// func main() {
-// 	file, _ := os.ReadFile("test.txt")
-
-// 	data := strings.Split(string(file), ",")
-
-// 	var old_fish lanternfish
-// 	// var new_fish lanternfish
-// 	for _, val := range data {
-// 		days, _ := strconv.Atoi(val)
-// 		new_fish := lanternfish{days_until_birth: days, Child: &old_fish}
-// 		old_fish := new_fish
-// 		fmt.Println(old_fish)
-// 	}
-
-// 	fmt.Println(old_fish.Child.days_until_birth)
-// 	// new_fish.days_until_birth = 8
-
-// 	// for day := 1; day <= final_day; day++ {
-// 	// 	for i, _ := range fish {
-// 	// 		if fish[i].the_miracle_of_life() {
-// 	// 			// fish = append(fish, new_fish)
-// 	// 		}
-// 	// 	}
-// 	// 	// fmt.Println(fish)
-// 	// }
-
-// }
 func main() {
+	start := time.Now()
+	file, _ := os.ReadFile("input.txt")
 
-	// get list of number of fish for each day, shift list over for each iteration til day is reached
+	data := strings.Split(string(file), ",")
+
+	var fish [9]int
+	for _, val := range data {
+		index, _ := strconv.Atoi(val)
+		fish[index]++
+	}
+
+	for day := 1; day <= final_day; day++ {
+		fish[7] += fish[0]
+
+		copy(fish[:], append(fish[1:], fish[0]))
+		// fmt.Printf("Day %d: %v\n", day, fish)
+	}
+
+	result := 0
+	for _, v := range fish {
+		result += v
+	}
+	fmt.Println(result)
+
+	elapsed := time.Since(start)
+	log.Printf("Binomial took %s", elapsed)
 }
